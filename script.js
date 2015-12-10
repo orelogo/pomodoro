0473420df19f5e7c3efec8e585477ae567eb0f81
orelovo <patso0@gmail.com>
2015-12-10 00:20:29 -0500

    Add work/break interval and interval controls
      - work/break intervals are color coded
      - can increase and decrease work and break intervals when timer paused
      - updated method comments in jsdoc style

--- script.js
+++ script.js
@@ -1,83 +1,208 @@
-var workMilliSec = 20000;             // time of pomodoro (work portion)
-var countdownTotal = workMilliSec;    // stores total countdown in milliseconds
-var remainingMilliSec = workMilliSec; // time remaining, used when paused
-var countdownHour, countdownMin, countdownSec, countdownMilli;
-var displayTime;                      // time in string format h:m:s.ms
-var timer;                            // store id for timer
+var workMillisec = 4000;             // time of pomodoro (work portion)
+var breakMillisec = 2000;             // time of break
 
-/* Run the timer */
-function runTimer() {
+var pausedMillisec = workMillisec; // time remaining, used when paused
+var countdownMillisec = pausedMillisec; // total countdown in milliseconds
+var displayHour, displayMin, displaySec, displayMillisec;
+var displayTime;                      // time in string format h:m:s.ms
+var timer;                            // id for timer
+var workInterval = true;     // true if work interval, false if break interval
+var timerRunning = false;    // true if timer running, false if not
+
+/**
+ * Start the timer.
+ */
+function startTimer() {
   var startTime = Date.now();  // time at start of timer
   timer = setInterval(function() {
-    countdownTotal = Math.round(remainingMilliSec - (Date.now() - startTime));
-    if (countdownTotal <= 0) { // stop timer if countdown reaches 0
-      clearInterval(timer);
-      zeroTime();
+    countdownMillisec = Math.round(
+      pausedMillisec - (Date.now() - startTime));
+    if (countdownMillisec <= 0) { // stop timer if countdown reaches 0
+      switchInterval();
     }
     printTime();
   }, 25);
 }
 
-/* Parse total millisecond countdown time to readable time */
+/**
+ * Parse total millisecond countdown time to readable time.
+ */
 function parseTotal() {
   var countdownRemaining;
-  countdownHour = Math.floor(countdownTotal / 1000 / 60 / 60);
-  countdownRemaining = countdownTotal - (countdownHour * 1000 * 60 * 60);
-  countdownMin = Math.floor(countdownRemaining / 1000 / 60);
-  countdownRemaining -= countdownMin * 1000 / 60;
-  countdownSec = Math.floor(countdownRemaining / 1000);
-  countdownRemaining -= countdownSec * 1000;
-  countdownMilli = Math.floor(countdownRemaining);
+  displayHour = Math.floor(countdownMillisec / 1000 / 60 / 60);
+  countdownRemaining = countdownMillisec - (displayHour * 1000 * 60 * 60);
+  displayMin = Math.floor(countdownRemaining / 1000 / 60);
+  countdownRemaining -= displayMin * 1000 / 60;
+  displaySec = Math.floor(countdownRemaining / 1000);
+  countdownRemaining -= displaySec * 1000;
+  displayMillisec = Math.floor(countdownRemaining);
 }
 
-/* Print time to screen */
+/**
+ * Print time to DOM.
+ */
 function printTime() {
   parseTotal();
   displayTime = ""; // time to be displayed
-  if (countdownHour > 0) // add hour column if necessary
-    displayTime += countdownHour + "h:";
-  displayTime += countdownMin + "m:" + countdownSec + "s." + countdownMilli
+  if (displayHour > 0) // add hour column if necessary
+    displayTime += displayHour + "h:";
+  displayTime += displayMin + "m:" + displaySec + "s." + displayMillisec
     + "ms"
-  $(".timer").text(displayTime);
+  $("#timer").text(displayTime);
 }
 
-/* Ensure countdown is zero */
+/**
+ * Pause timer.
+ */
+function pauseTimer() {
+  clearInterval(timer);
+  pausedMillisec = countdownMillisec; // store remaining time
+}
+
+/**
+ * Toggle between pause and unpause timer.
+ */
+function toggleTimer() {
+  if (timerRunning) {
+    pauseTimer();
+    $("#timer-toggle").text("Start");
+    timerRunning = false;
+  }
+  else {
+    startTimer();
+    $("#timer-toggle").text("Pause");
+    timerRunning = true;
+  }
+}
+
+/**
+ * Set countdown to 0. May not be necessary.
+ */
 function zeroTime() {
-  countdownTotal = 0;
+  countdownMillisec = 0;
   parseTotal();
 }
 
-/* Pause timer */
-function pauseTimer() {
+/**
+ * Reset time to work beginning of work interval
+ */
+function resetTimerToWork() {
   clearInterval(timer);
-  timer = undefined;
-  remainingMilliSec = countdownTotal; // store remaining time
+  timerRunning = false; // timer stopped
+
+  workInterval = true;  // reset to work interval
+  // reset time for timer and for display
+  countdownMillisec = pausedMillisec = workMillisec;
+  $("#timer").css("color", "green");
+  $("#timer-toggle").text("Start");
+  printTime();
 }
 
-function resetTimer() {
+/**
+ * Reset the timer to break beginning of break interval. Used in
+ * breakIncrease() and breakDecrease() methds during a paused break interval.
+ */
+function resetTimerToBreak(interval) {
   clearInterval(timer);
-  timer = undefined;
+
+  workInterval = false;  // reset to break interval
   // reset time for timer and for display
-  remainingMilliSec = countdownTotal = workMilliSec;
+  countdownMillisec = pausedMillisec = breakMillisec;
+  $("#timer").css("color", "red");
   printTime();
-  $("#timer-control").text("Start");
+}
+
+/**
+ * Stop timer, switch between work/break interval and start a new timer.
+ */
+function switchInterval() {
+  clearInterval(timer); // stop timer
+  if (workInterval) {   // chage to break interval
+    countdownMillisec = pausedMillisec = breakMillisec;
+    workInterval = false;
+    $("#timer").css("color", "red");
+  }
+  else { // change to work interval
+    countdownMillisec = pausedMillisec = workMillisec;
+    workInterval = true;
+    $("#timer").css("color", "green");
+  }
+  startTimer(); // start new timer
+}
+
+/**
+ * Increase work interval time. Can only change if timer isn't running.
+ * If in the middle of a work interval, the interval will be reset with the
+ * new time displayed.
+ */
+function workIncrease() {
+  if (!timerRunning) {
+    workMillisec += 1000;
+    // if on break work, reset timer to work
+    if (workInterval)
+      resetTimerToWork();
+  }
+}
+
+/**
+ * Decrease work interval time. Can only change if timer isn't running.
+ * If in the middle of a work interval, the interval will be reset with the
+ * new time displayed.
+ */
+function workDecrease() {
+  if (!timerRunning) {
+    workMillisec -= 1000;
+    // work time cannot be less than 0
+    if (workMillisec <= 0)
+      workMillisec = 0;
+    // if on work interval, reset timer to work
+    if (workInterval)
+      resetTimerToWork();
+  }
+}
+
+/**
+ * Increase break interval time. Can only change if timer isn't running.
+ * If in the middle of a break interval, the interval will be reset with the
+ * new time displayed.
+ */
+function breakIncrease() {
+  if (!timerRunning) {
+    breakMillisec += 1000;
+    // if on break interval, reset timer to break
+    if (!workInterval)
+      resetTimerToBreak();
+  }
+}
+
+/**
+ * Decrease break interval time. Can only change if timer isn't running.
+ * If in the middle of a break interval, the interval will be reset with the
+ * new time displayed.
+ */
+function breakDecrease() {
+  if (!timerRunning) {
+    breakMillisec -= 1000;
+    if (breakMillisec <= 0)
+      breakMillisec = 0;
+    // if on break interval, reset timer to break
+    if (!workInterval)
+      resetTimerToBreak();
+  }
 }
 
 $(document).ready(function() {
   printTime();
 
   // start/pause button
-  $("#timer-control").click(function() {
-    if (timer === undefined) {
-      runTimer();
-      $(this).text("Pause");
-    } else {
-      pauseTimer();
-      $(this).text("Start");
-    }
-  });
-
+  $("#timer-toggle").click(toggleTimer);
   // reset button
-  $("#timer-reset").click(resetTimer);
+  $("#timer-reset").click(resetTimerToWork);
+
+  // interval control buttons
+  $("#work-increase").click(workIncrease);
+  $("#work-decrease").click(workDecrease);
+  $("#break-increase").click(breakIncrease);
+  $("#break-decrease").click(breakDecrease);
 
 });
